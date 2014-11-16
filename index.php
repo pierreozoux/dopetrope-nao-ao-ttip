0b2cb691d2a004ba5a5caf6b7df2264d3c931b3f
Pascal <pascal@lino-design.com>
2014-11-16 21:34:38 +0000

    Add specific title for category / search result / tags / Autor

--- index.php
+++ index.php
@@ -12,8 +12,40 @@
     </div>
     
     <!-- Content -->
-    <div class="row 8u">
+    <div class="row 8u" >
+      <h1 class="list-post-title">
+          <?php 
+          if (is_category() && single_cat_title( '', false ) != 'Noticias') {
+              printf( 'Categoria "%s"', '<span>' . single_cat_title( '', false ) . '</span>' );
+          }
+          if (is_tag()) {
+              printf( 'Tag "%s"', '<span>' . single_tag_title( '', false ) . '</span>' ); 
+          }      
+          if (is_author()) {
+              printf( 'Autor: %s', '<a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a>' ); 
+          }  
+          if (is_search()) {
+              printf( 'Resultado da pesquisa "%s"', '<span>' . get_search_query() . '</span>' );
+          }
+          ?>
+      </h1>      
       <!– WP LOOP STARTS/ENDS –>
+      <?php
+        if(is_search() && !have_posts()) { ?>
+            <article id="post-0" class="post no-results not-found">
+                    <header class="entry-header">
+                            <h1 class="entry-title">Não há resultados</h1>
+                    </header>
+
+                    <div class="entry-content">
+                            <p>Desculpe, mas nada combina com seus critérios de pesquisa. Por favor, tente novamente com palavras-chave diferentes</p>
+                    </div>
+            </article><!-- #post-0 -->      
+            <?php
+        }
+      ?>
+      <div class="clear"></div>
+            
       <?php while (have_posts()) : the_post(); ?>
         <div class="6u archive-box">
           <section class="box flyer-box">
