4132e2ae6a25fff392f8f8b42c166c9bea2aa4fe
Pierre Ozoux <pierre@ozoux.net>
2015-02-25 18:20:20 +0000

    Adds footer function (To support piwik)

--- footer.php
+++ footer.php
@@ -121,5 +121,6 @@ $('.date').each(function(index, element) {
     $(element).html($(element).html().replace(/(\d\d)/, "<strong>$1</strong>"));
 });
 </script>
+ <?php wp_footer(); ?> 
 </body>
 </html>
