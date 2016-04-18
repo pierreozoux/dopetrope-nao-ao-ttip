03562c825b48c02baec6a92fc50c60273cf43b29
camillesim <camille.simermann@gmail.com>
2016-04-18 13:19:38 +0100

    im hp

--- page-home.php
+++ page-home.php
@@ -10,7 +10,7 @@
 					<!-- Banner -->
 						<section id="banner">
 
-              <a href="https://www.nao-ao-ttip.pt/conversa-15042016-as-2130-guimaraes/"></a>
+              <a href="https://www.nao-ao-ttip.pt/zona-livre-do-ttip/"></a>
 							<!--<header style="color: transparent; background-color: transparent;">
 								<h2 style="color: transparent;">Não ao Tratado Transatlântico</h2>
 								<p style="color: transparent;">Plataforma portuguesa para análise crítica ao TTIP</p>
